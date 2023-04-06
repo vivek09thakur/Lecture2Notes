72c222dc5b70514b917a25bb76d8ee9d5fcb060d
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-04-06 17:56:13 +0530

    upgraded with vosk

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-
-import speech_recognition as sr
+from vosk import Model,KaldiRecognizer
+import pyaudio
 from fpdf import FPDF
 import time
 import os
+import json
 
 
+# Speech Recognition Using Vosk
+model = Model('./vosk/vosk-model-small-en-in-0.4')
+mic = pyaudio.PyAudio()
+recognizer = KaldiRecognizer(model, 16000)
+stream = mic.open(rate=16000, channels=1, format=pyaudio.paInt16, input=True, frames_per_buffer=8192)
+stream.start_stream()
 
+    
 def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source,phrase_time_limit=10)
-    try:
-        query = r.recognize_google(audio, language='en-in')
-        query = query.lower()
-        print(f'words recognised : {query}')
-        time.sleep(1)
-    except Exception as e:
-        print(e)
-        print("Unable to recognize")
-        return "none"
+    
+    frames = []
+    for i in range(0, int(16000 / 8192 * 3)):
+        data = stream.read(8192)
+        frames.append(data)
+    data = b''.join(frames)
 
-    return query
+    try:
+        if recognizer.AcceptWaveform(data):
+            results = recognizer.Result()
+            user_input = json.loads(results)['text']
+            return user_input
+        else:
+            return None
+    except Exception as e:
+        print("Unable to recognize waveform")
+        print(e)
+
+
+def  clear_console():
+    return os.system('cls')
+
 
 pdf = FPDF()
 pdf.add_page()
@@ -36,21 +51,27 @@ except FileNotFoundError:
 # Add the existing text to the PDF
 pdf.multi_cell(0, 10, text)
 
-while True:
-    
-    data = listen().lower()
-    with open("notes.txt", "a") as f:
-        f.write(f"{data}\n")
-        pdf.multi_cell(0,10,data)
-        
-    if 'class over' in data:
-        break
-    
-    
-    # Convert the text file to PDF after writing
-with open("notes.txt", "r") as file:
-    text = file.read()
 
-with open("notes.pdf", "w") as file:
-    pdf.output("notes.pdf",'F')
+if __name__=='__main__':
     
+    clear_console()
+    while True:
+        
+        data = listen()
+        print('User Said : '+str(data))
+        if data is not None:
+            with open("notes.txt", "a") as f:
+                f.write(f"{data}\n")
+                pdf.multi_cell(0, 10, data)
+
+            if 'class over' in data:
+                break
+
+            
+        # Convert the text file to PDF after writing
+    with open("notes.txt", "r") as file:
+        text = file.read()
+
+    with open("notes.pdf", "w") as file:
+        pdf.output("notes.pdf",'F')
+
